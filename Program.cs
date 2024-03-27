38a7d8f5e03f4d377f082c18307760655c65c43d
Student MyProject <atharvaborikar4784@gmail.com>
2024-03-27 23:51:21 +0530

    hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ConsoleApp1
+{
+    public Program()
+    {
+        static void Main(string[] args)
+        {
+            ConsoleApp1.WriteLine("Hello World");
+        }
+    }
+}
